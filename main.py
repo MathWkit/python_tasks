37687c2e031444dc47dfbe0d10ade56c3b451f41
Mathew <mathwkit@gmail.com>
2025-03-20 12:47:02 +0300

    the logic of cyclic is made #2

--- main.py
+++ main.py
@@ -8,6 +8,41 @@ def read_array(filename):
             return []
         return list(map(int, content.split()))
 
-def write_array(filename, array):
+def write_matrix(filename, matrix):
     with open(filename, "w") as file:
-        file.write(' '.join(map(str, array)))
+        file.write(' '.join(map(str, matrix)))
+
+def cyclic_shift(matrix, n, setting):
+    if not matrix or not matrix[0]:
+        return
+
+    if setting == 'r':
+        n = n % len(matrix)
+        return matrix[-n:] + matrix[:-n]
+    elif setting == 'c':
+        n = n % len(matrix[0])
+        return [row[-n:] + row[:-n] for row in matrix]
+        return
+    else:
+        print("Настройка должна быть r (rows) или c (columns)")
+
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 0, 1, 2]
+]
+
+print("Исходная матрица:")
+for row in matrix:
+    print(row)
+
+print("\nСдвиг строк вниз на 2:")
+shifted_matrix = cyclic_shift(matrix, 2, 'r')
+for row in shifted_matrix:
+    print(row)
+
+print("\nСдвиг столбцов вправо на 2:")
+shifted_matrix = cyclic_shift(matrix, 2, 'c')
+for row in shifted_matrix:
+    print(row)
